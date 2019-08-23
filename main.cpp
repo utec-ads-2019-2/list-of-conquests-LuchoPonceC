ac8d46f8cb8aca969c0e5b32c629ae7d20aa2e45
LuchoPonce <luis.ponce@utec.edu.pe>
2019-08-22 22:00:15 -0700

    Final commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    return 0;
+    
+	int cantidad;
+	string pais,persona1,persona2;
+
+	cin>>cantidad;
+
+	map<string,set<string>> paises;
+
+	for(int i=0; i<cantidad;i++){
+		cin>>pais;
+		getline(cin,persona1);
+		paises[pais].insert(persona1);
+	}
+	
+	for(auto valores : paises){
+		std::cout<<valores.first<<" "<<valores.second.size()<<endl;
+	}
+
 }
